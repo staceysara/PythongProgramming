94e5109db7a65ae2802b2cd0694ef8224b4ff225
staceysara <sarakjykjy@naver.com>
2015-09-02 13:46:27 +0900

    Project0902
    Project0902

---
+++ PythonProject0902/PythonProject0902/PythonProject0902.py
@@ -0,0 +1,33 @@
+﻿#print("Here are the numbers!"+\
+#    "The first is{0:d} The second is {1:4d}".format(7,8))
+#------------------
+#print("한글")
+#salary = input("Please enter your salary:")
+#bonus = input("Please enter your bonus:")
+#payCheck = salary+bonus
+#payCheck = float(salary)+float(bonus)
+#print(payCheck)
+#print(type(payCheck))
+#------------------
+
+#print("="*10)
+#name = "greenjoa"
+#print(name[0])
+#print(name[1])
+#print(name[-2])
+#------------------
+#info = "201311203greenjoa"
+#sid = info[:9]
+#sname = info[9:]
+#print(sid +" " + sname)
+#------------------
+#a = "I eat %10s apples" % "five"#오른쪽정렬
+#print(a)
+#a = "I eat %-10s apples"% "five"#왼쪽정렬
+#print(a)
+#a = "Error is %d%%"%98#진짜%를쓰고싶음%%를쓰기
+#print(a)
+#------------------
+command = input("종료할까요? ")#대문자나 소문자를 입력할지 모르니 하나로 바꿔주기
+command = command.lower()
+print(command)
