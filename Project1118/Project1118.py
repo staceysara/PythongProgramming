285ca83c74d7b1035e137f8a6dc630594f9a5c13
staceysara <sarakjykjy@naver.com>
2015-11-18 14:51:13 +0900

    Project1118
    Project1118

---
+++ Project1118/Project1118.py
@@ -0,0 +1,28 @@
+﻿from flask import Flask, render_template,url_for
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    data=[dict(href="http://www.naver.com",caption="네이버"),dict(href="http://www.google.com",caption="구글")]
+    return render_template('hello.html',items=data)
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!'
+
+#상속 등등 실습한거 더 있는데 안되었음
+#이거 실습해보기(상속, 이름 보내기? 등)
+#@app.route('hello/<name>')
+#def hello(name=None):
+#    return render_template('hello.html',name=name)
+#----------
+#@app.route('/data1/')
+#def hello_world():
+#    data={
+#        'title':'Hello',
+#        'name':'greenjoa'
+#        }
+#    return render_template('hello.html',**data)
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
+    
